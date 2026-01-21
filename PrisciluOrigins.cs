d8f596ca9459725fc24b336c9827b3cf459c1314
CyberByteCraft <xgina@pushedv.de>
2026-01-21 23:58:25 +0100

    Release v6.3.0: Update M80A1 price and finalize release

--- PrisciluOrigins.cs
+++ PrisciluOrigins.cs
@@ -19,7 +19,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "Priscilu_Origins_v2";
     public override string Author { get; init; } = "Reis | Update/Contributor: Anigx";
     public override List<string>? Contributors { get; init; } = ["Anigx"];
-    public override SemanticVersioning.Version Version { get; init; } = new("6.2.10");
+    public override SemanticVersioning.Version Version { get; init; } = new("6.3.0");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.11");
     public override List<string>? Incompatibilities { get; init; } = [];
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = null;
